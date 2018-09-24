a6ed3488b9e6208d1533fae6eb79eab5c90f3d36
Saulo Reis <saluart@gmail.com>
2018-09-24 22:33:28 +0000

    Start implementing the insert funcionality

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelpcamp")
 app.use(bodyParser.urlencoded({extended:true}));
 
 let camps = [
         {name:"PachaMama Camping", image: "https://media-cdn.tripadvisor.com/media/photo-s/0f/58/8f/89/area-de-camping-com-vista.jpg"},
         {name:"Camping Salto do Corumbá", image: "https://macamp.com.br/guia/wp-content/uploads//arquivos/guia/arquivos/1269/imagens/1714bab370dc2f59f935f00581657f371.jpg"},
         {name:"Jangadão Ecológico", image: "https://catracalivre.com.br/wp-content/uploads/2017/07/canions_do_rio_Araguaia5.jpg" },
-    ]
+    ];
+    
+const campgroundSchema = mongoose.Schema({
+    name: String,
+    image: String
+})
+
+const Campground = mongoose.model("Campground", campgroundSchema);
+
+camps.forEach(camp => {
+    Campground.create(camp, function(err, result){
+        if(err){
+            console.log("Something went wrong!");
+        } else {
+            console.log("New campground added like follow:");
+            console.log(result);
+        }
+    })
+})
 
 app.set("view engine", "ejs");
 
